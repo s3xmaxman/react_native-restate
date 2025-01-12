ae06b4e89764a9a30e133a84fa12510805d1f381
s3xmaxman <badside3919@gmail.com>
2025-01-12 13:58:50 +0900

    Enhance Tailwind configuration and update layout component: Added paths for component files in tailwind.config.js, defined custom font families and color palette. Refactored _layout.tsx to load Rubik font family asynchronously and manage splash screen visibility based on font loading state.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,31 @@
 import { Slot } from "expo-router";
+import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 // Import your global CSS file
 import "./global.css";
 
-export default Slot;
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require("../assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("../assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("../assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("../assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("../assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("../assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Slot />;
+}
