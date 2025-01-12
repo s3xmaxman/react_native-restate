22400ac4193e326b78436fca5b0c3bf3e9162337
s3xmaxman <badside3919@gmail.com>
2025-01-12 14:13:49 +0900

    Refactor layout and update sign-in screen: Replaced Slot component with Stack in _layout.tsx for improved navigation. Removed the unused index.tsx file. Enhanced sign-in screen with SafeAreaView and ScrollView, incorporating an onboarding image for better user experience.

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
+import images from "@/constants/images";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default SignIn;
-
-const styles = StyleSheet.create({});
