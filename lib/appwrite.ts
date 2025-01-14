776549d5c8b3e34edbb683a658e4de54ea353e06
s3xmaxman <badside3919@gmail.com>
2025-01-14 09:25:28 +0900

    No changes detected in the current diff; no updates made to the project files.

---
+++ lib/appwrite.ts
@@ -0,0 +1,53 @@
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  OAuthProvider,
+  Storage,
+} from "react-native-appwrite";
+import * as Linking from "expo-linking";
+
+export const config = {
+  platform: "com.restate.s3xmaxman",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+  bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+
+export async function login() {
+  try {
+    const redirectUri = Linking.createURL("/");
+
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+
+    if (!response) {
+      throw new Error("Something went wrong");
+    }
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
